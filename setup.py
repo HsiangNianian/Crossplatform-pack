26e5f6639cb3b4bfbdd98d3500009f1e2be1580d
简律纯 <admin@jyunko.cn>
2023-04-10 11:20:22 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     ],
     entry_points={
         "console_scripts": [
-            "myapp = app.main:main"
+            "myapp = Crossplatform-pack.app.main:main"
         ]
     },
     license="MIT",
